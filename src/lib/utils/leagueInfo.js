d0ebbe7c39e39df2df88c8a3b726d3ed0b805eba
BennieOP <112830271+BennieOP@users.noreply.github.com>
2022-09-04 18:49:53 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -9,10 +9,8 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 export const homepageText = `
   <p>Welcome, Sigmas, to Sigma Fantasy. As we all know, the one true indicator of a mans worth is his fantasy football record. Big calves, millions in the bank, and a charming smile are nice, but nothing compares to the clout acheived from dunking on your friends for 14 weeks to take home the fantasy football trophy. This is the way.</p>
   <p>This site was mainly just an experiment I felt like doing, but I think its pretty sweet. Everything is pulled from the sleeper app so the data should be up to date, but there are a few extra tabs to play around with. If you think there is any cool feature that could be added, let me know and I can try to add it.  </p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p></p>
+  
 `;
 
 /*   STEP 3   */
